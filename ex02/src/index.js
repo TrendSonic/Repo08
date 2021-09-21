ba0240e2fe4ecd0f9e207132571d6466f8fb296e
Edin <edinosmic77@gmail.com>
2021-09-21 06:37:44 +0200

    adding ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+    console.log(name, age);
+}
+
+functionPrintInfo("John", 23); // prints to the console
+functionPrintInfo("Michael", 43); // prints to the console
+// Write code above this line
+
+module.exports = functionPrintInfo;
