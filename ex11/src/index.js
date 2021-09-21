e7e8592c08204ea897215b0817d21b75f14e32b7
Edin <edinosmic77@gmail.com>
2021-09-21 08:48:54 +0200

    adding ex11

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -11,12 +11,12 @@ function mathTest(x, y) {
     // Only change code below this line
 }
 
-mathTest(2, 2);
-mathTest(2, 2);
-mathTest(-2, 2);
-mathTest(2, -2);
-mathTest(2, 8);
-mathTest(3, 3);
-mathTest(0, 0);
+console.log(mathTest(2, 2));
+console.log(mathTest(2, 2));
+console.log(mathTest(-2, 2));
+console.log(mathTest(2, -2));
+console.log(mathTest(2, 8));
+console.log(mathTest(3, 3));
+console.log(mathTest(0, 0));
 // Only change code above this line
 module.exports = mathTest;
