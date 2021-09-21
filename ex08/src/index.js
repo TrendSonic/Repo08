5555aaa2ac749b843bead58febce50cf438d9eeb
Edin <edinosmic77@gmail.com>
2021-09-21 07:30:42 +0200

    adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+var total = 0;
+
+function funcWithArg(digit) {
+    return sum = (digit + 9) / 5;
+}
+
+funcWithArg(6);
+total = sum;
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
